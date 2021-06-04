08d6e29670a924728f1d9513e4cdaeb000d98f1a
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-06-04 14:37:16 -0600

    set up views

--- GoodTreats/Models/TreatFlavor.cs
+++ GoodTreats/Models/TreatFlavor.cs
@@ -5,10 +5,10 @@ namespace GoodTreats.Models
   public class TreatFlavor
   {
     public int TreatFlavorId { get; set; }
-    public int TreatId { get; set; }
     public int FlavorId { get; set; }
-    public virtual Treat Treat { get; set; }
+    public int TreatId { get; set; }
     public virtual Flavor Flavor { get; set; }
+    public virtual Treat Treat { get; set; }
 
   }
 }
